e0103305c8dc06c65dce8cc6f3ffba42455f3390
odoggett <oliviatheresadoggett@gmail.com>
2015-10-06 12:07:35 -0400

    first draft exercise 3

--- exercise3.py
+++ exercise3.py
@@ -12,21 +12,55 @@ __email__ = "ses@drsusansim.org"
 __copyright__ = "2015 Susan Sim"
 __license__ = "MIT License"
 
+#Interactively queries the user with yes/no questions to identify a
+#possible issue with a car.
 
+# Inputs:
 def diagnose_car():
-    """
-    Interactively queries the user with yes/no questions to identify a
-    possible issue with a car.
+    print ("Is the car silent when you turn the key?")
+    ans1 = raw_input("Press [Y]Yes or [N]No")
+    if ans1 == "y":
+        print ("Are the battery terminals corroded?")
+        ans2 = raw_input("Press [Y]Yes or [N]No")
+        if ans2 == "y":
+            print ("Clean terminals and try starting again.")
+        else:
+            print ("Replace cables and try again.")
+    else:
+        print ("Does the car make a clicking noise?")
+        ans3 = raw_input("Press [Y]Yes or [N]No")
+        if ans3 == "y":
+            print ("Replace the battery.")
+        else:
+            print ("Does the car crank up but fail to start?")
+            ans4 = raw_input("Press [Y]Yes or [N]No")
+            if ans4 == "y":
+                print ("Check spark plug connections.")
+            else:
+                print ("Does the engine start and then die?")
+                ans5 = raw_input("Press [Y]Yes or [N]No")
+                if ans5 == "y":
+                    print ("Does your car have fuel injection?")
+                    ans6 = raw_input("Press [Y]Yes or [N]No")
+                    if ans6 == "y":
+                        print ("Get it in for service.")
+                    else:
+                      print ("Check to ensure the choke is opening and closing.")
+                else:
+                    print ("Engine is not getting enough fuel. Clean fuel pump.")
 
-    Inputs:
 
-    Expected Outputs:
 
-    Errors:
 
-    """
 
-    print("The battery cables may be damaged. Replace cables and try again.")
+
+#    Expected Outputs:
+
+#    Errors:
+
+
+
+  #  print("The battery cables may be damaged. Replace cables and try again.")
 
 
 diagnose_car()
