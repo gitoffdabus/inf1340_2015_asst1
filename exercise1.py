2245bff615d2f5b7a3adef5d376488fbdc0aca35
odoggett <oliviatheresadoggett@gmail.com>
2015-10-06 11:23:22 -0400

    first draft exercise 1

--- exercise1.py
+++ exercise1.py
@@ -12,10 +12,22 @@ __email__ = "ses@drsusansim.org"
 __copyright__ = "2015 Susan Sim"
 __license__ = "MIT License"
 
+#calculate cost of total shares
+#calculate commission costs
+#deduct the amount she paid her stockbroker
 
-money = 100.00
-print(money)
+number_of_shares = 2000
+initial_share = 900
+sales_share = 942.75
+commission_percentage = .03
+initial_commission = number_of_shares * initial_share * commission_percentage
+initial_cost = number_of_shares * initial_share + initial_commission
+sales_commission = number_of_shares * sales_share * commission_percentage
+sales_price = number_of_shares * sales_share - sales_commission
+money = sales_price - initial_cost
+print ("Lakshi is at a balance of %d after selling her stock and paying her broker twice" %money)
 
-cheque = 10
+
+
 
 
